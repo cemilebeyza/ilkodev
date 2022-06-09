6f91c837c2d9b1dbd7390a1f76deef1b9709b5b6
Cemile Beyza Türker <84451617+cemilebeyza@users.noreply.github.com>
2022-06-09 14:49:36 +0300

    Create harrypotter.py

---
+++ harrypotter.py
@@ -0,0 +1,68 @@
+class Wizard:
+    def _init_(self, name, color):
+        self.name = name
+        self.color = color
+
+class Gryffindor(Wizard):
+    def _init_(self, name):
+        Wizard._init_(self, name, 'red')
+
+    def nameOfHouse(self):
+        return "Gryffindor"
+    
+    def getValues(self):
+        return ["Bravery", "helping others", "chivalry"]
+    
+    
+class Hufflepuff(Wizard):
+    def _init_(self, name):
+        Wizard._init_(self, name, 'yellow')
+
+    def nameOfHouse(self):
+        return "Hufflepuff"
+    
+    def getValues(self):
+        return ["Hard work", "patience", "loyalty", "fair play"]
+
+class Ravenclaw(Wizard):
+    def _init_(self, name):
+        Wizard._init_(self, name, 'blue')
+    
+    def nameOfHouse(self):
+        return "Ravenclaw"
+    
+    def getValues(self):
+        return ["Intelligence", "knowledge", "planning ahead", "wit"]
+
+class Slytherin(Wizard):
+    def _init_(self, name):
+        Wizard._init_(self, name, 'green')
+    
+    def nameOfHouse(self):
+        return "Slytherin"
+    
+    def getValues(self):
+        return ["Ambition", "cunningness", "heritage", "resourcefulness"]
+
+while True:
+    name = input("Enter your name: ")
+    color = input("Favourite Color: ")
+
+    if (color == "red"):
+        wizard = Gryffindor(name)
+    
+    elif (color == "yellow"):
+        wizard = Hufflepuff(name)
+    
+    elif (color == "blue"):
+        wizard = Ravenclaw(name)
+    
+    elif (color == "green"):
+        wizard = Slytherin(name)
+    
+    else:
+        print("Wrong color")
+        break
+    
+    print("Your House: " + wizard.nameOfHouse())
+    print("Your Values: " + str(wizard.getValues()))
